b01b16abcf17d2f1ec5cefb820cea7297c980e8b
fnz17 <artemtopintheworld@gmail.com>
2020-11-22 17:49:37 +0100

    update 2.0

--- modal.js
+++ modal.js
@@ -1,3 +1,11 @@
+document.querySelectorAll('[data-click-modal]').forEach( function(element){
+    element.addEventListener('click', function(){
+        OnModalClick();
+    } 
+    )
+}
+
+)
 function OnModalClick(){
     document.querySelector('.modal').classList.toggle("modal_show");
     document.querySelector('.wrapper').classList.toggle("wrapper-on");
